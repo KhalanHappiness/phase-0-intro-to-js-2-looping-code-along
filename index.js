3061557f61c1d37c764d69ef127d1b8c391e9fbf
happiness <happinesskhalan@gmail.com>
2025-02-28 03:14:25 +0300

    for and while loops complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+
+function writeCards(friends=["Samuel", " Lydia", "Esther"], graduation){
+    const newArray = []
+    for(let i=0; i< friends.length; i++){
+
+        newArray.push(`Thank you, ${friends[i]}, for the wonderful ${graduation} gift!`)
+    }
+    return newArray
+
+}
+
+function countDown(){
+    let i =10;
+
+    while(i>=0){
+        console.log(i)
+        i--
+        
+    }
+
+
+}
+
+countDown()
